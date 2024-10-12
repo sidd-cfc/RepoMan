e065f15fa04fc5096a8ba50e43c41e5d33202742
sidd-cfc <sidd2.cfc@gmail.com>
2024-10-12 23:47:03 +0530

    add while and modify elif

---
+++ Python_Stuff/While.py
@@ -0,0 +1,17 @@
+# spam=1
+# while spam<5:
+#     print('Hello world')
+#     spam=spam+1  
+
+# name=''
+# while name !='Siddhesh':
+#     print('Please type your name')
+#     name=input()
+# print('Thank you')
+
+spam = 0                    # Step 1: Initial value of 'spam' is set to 0
+while spam < 5:              # Step 2: The loop will keep running as long as 'spam' is less than 5
+    spam = spam + 1          # Step 3: Add 1 to 'spam' each time the loop runs
+    if spam == 3:            # Step 4: Check if 'spam' is equal to 3
+        continue             # Step 5: If 'spam' is 3, skip the rest of the code and go to the next loop iteration
+    print('spam is ' + str(spam))  # Step 6: Print 'spam is' followed by the current value of 'spam', except when spam is 3
--- Python_Stuff/eLiF.py
+++ Python_Stuff/eLiF.py
@@ -1,9 +1,11 @@
-Goals=9
-if Goals>20:
-    print('Good attacker')
-elif Goals>10:
-    print('You are an Okish attacker')
-elif Goals<10:
-    print('You are trash bro')
+Goals = 25  # Set Goals to a non-numeric value
+
+if isinstance(Goals, int):  # Check if Goals is an integer
+    if Goals >= 20:
+        print('Good attacker')
+    elif Goals >= 10:
+        print('You are an Okish attacker')
+    elif Goals < 10:
+        print('You are trash bro')
 else:
     print('Just retire')
