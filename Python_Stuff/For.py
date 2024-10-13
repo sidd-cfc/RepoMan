149d36a5d71857a70401b77d90720b9303903232
sidd-cfc <sidd2.cfc@gmail.com>
2024-10-13 22:01:53 +0530

    mod While and add For loop files

---
+++ Python_Stuff/For.py
@@ -0,0 +1,12 @@
+# print('My name is')
+# for i in range(5):
+#     print('Siddhesh five times '+str(i))
+
+# print('Welcome to the event!')
+# for i in range(5):
+#     print('Person ' + str(i+1) + ' has checked in.')
+
+Players=['Cole Palmer','Nkunku','Malo Gusto','Cucurella','Colwill','Joao Felix']
+print('Current Favourite Chelsea FC players:')
+for player in Players:
+    print(player)
--- Python_Stuff/While.py
+++ Python_Stuff/While.py
@@ -9,9 +9,20 @@
 #     name=input()
 # print('Thank you')
 
-spam = 0                    # Step 1: Initial value of 'spam' is set to 0
-while spam < 5:              # Step 2: The loop will keep running as long as 'spam' is less than 5
-    spam = spam + 1          # Step 3: Add 1 to 'spam' each time the loop runs
-    if spam == 3:            # Step 4: Check if 'spam' is equal to 3
-        continue             # Step 5: If 'spam' is 3, skip the rest of the code and go to the next loop iteration
-    print('spam is ' + str(spam))  # Step 6: Print 'spam is' followed by the current value of 'spam', except when spam is 3
+# spam = 0                    # Step 1: Initial value of 'spam' is set to 0
+# while spam < 5:              # Step 2: The loop will keep running as long as 'spam' is less than 5
+#     spam = spam + 1          # Step 3: Add 1 to 'spam' each time the loop runs
+#     if spam == 3:            # Step 4: Check if 'spam' is equal to 3
+#         continue             # Step 5: If 'spam' is 3, skip the rest of the code and go to the next loop iteration
+#     print('spam is ' + str(spam))  # Step 6: Print 'spam is' followed by the current value of 'spam', except when spam is 3
+    
+    
+attempts = 0
+while attempts < 3:
+    password = input("Enter password: ")
+    if password == "correctpassword":
+        print("Access granted")
+        break  # Stop the loop if the correct password is entered
+    attempts=attempts + 1
+else:
+    print("Too many attempts, access denied")
