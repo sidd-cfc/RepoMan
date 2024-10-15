cc1a9f1082b194df22fb3a6739cad4c549f80377
sidd-cfc <sidd2.cfc@gmail.com>
2024-10-16 00:45:45 +0530

    adding stuff to func

--- Python_Stuff/Functions.py
+++ Python_Stuff/Functions.py
@@ -2,6 +2,26 @@
 # print('Hello')
 # sys.exit()
 # print('Goodbye')
+# import pyperclip
 
 
-import pyperclip
+# def hello():
+#     print('kasaa kaay!')
+#     print('Kaay boltos bhaavaa!!')
+    
+# hello()
+# hello() 
+
+# def hello(name):
+#     print('Hello ' +name)
+    
+# hello('Siddhesh')
+# hello('Sidd')
+
+# def plusOne(number):
+#     return number + 1
+
+# newNumber = plusOne(5)
+# print(newNumber)
+
+'Hello'
